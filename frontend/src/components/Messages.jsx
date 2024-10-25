2d4a8a761dd1c7f3be1a093b6ac9ad5218c1eb89
Coding-Rod <sergiorodrigofernandeztesta@gmail.com>
2024-10-24 21:47:21 -0400

    ✨ [ADD] Timestamp

--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -1,4 +1,9 @@
-export function Messages({messages}) {
+export function Messages({ messages }) {
+  const convertTimeFormat = (timestamp) => {
+    const date = new Date(timestamp);
+    return date.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+  };
+
   return (
     <div className="container mx-auto my-40 px-4 sm:px-10">
       {messages.map(message => (
@@ -13,6 +18,7 @@ export function Messages({messages}) {
             style={{ maxWidth: '90%', sm: { maxWidth: '60%' } }}
           >
             <p>{message.text}</p>
+            <p className={`text-xs mt-2 ${message.sender === 'user' ? 'text-white' : 'text-gray-800'}`}>{convertTimeFormat(message.timestamp)}</p>
           </div>
         </div>
       ))}
